67bf43351702a105007f10adfcecdbe5795c99a0
Michael Alegrado <alemike03@gmail.com>
2025-03-06 15:43:50 +0800

    employee zod schema

--- src/modules/user/ctrl.get.ts
+++ src/modules/user/ctrl.get.ts
@@ -3,6 +3,6 @@ import resp from "objectify-response";
 import prisma from "../prisma";
 
 export const userGetAllController = async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany({});
+  const users = await prisma.user.findMany({ omit: { password: true } });
   return resp(res, users)
 }
