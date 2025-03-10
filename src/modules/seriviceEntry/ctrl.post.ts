0ff7f35745c5743af2afe41c44edc97605b016eb
Michael Alegrado <alemike03@gmail.com>
2025-03-10 17:24:24 +0800

    up

--- src/modules/seriviceEntry/ctrl.post.ts
+++ src/modules/seriviceEntry/ctrl.post.ts
@@ -2,11 +2,26 @@ import resp from "objectify-response";
 import { NextFunction, Response } from "express";
 import prisma from "../prisma";
 import { ServiceEntryCreateRequest } from "./schema";
+import { Prisma } from "@prisma/client";
 
-export const serviceEntryCreateController = async (req: ServiceEntryCreateRequest, res: Response, next: NextFunction) => {
-  const serviceEntry = await prisma.service_entry.create({
-    data: req.body,
-  })
-
-  resp(res, serviceEntry)
+export const serviceEntryCreateController = async (
+  req: ServiceEntryCreateRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  prisma.service_entry
+    .create({
+      data: req.body,
+    })
+    .then((seriviceEntry) => {
+      resp(res, seriviceEntry);
+    })
+    .catch((e) => {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2003" && e.meta?.["field_name"] === "dailyRecordId") {
+          return resp(res, "Daily Record does not exist.", 400);
+        }
+      }
+      next(e);
+    });
 };
