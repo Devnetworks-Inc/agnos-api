9594bc2417ca09d9986b9f24b7c894b39b92c2fb
Michael Alegrado <alemike03@gmail.com>
2025-03-31 20:16:41 +0800

    auto create hotel services

--- src/modules/hotel/ctrl.post.ts
+++ src/modules/hotel/ctrl.post.ts
@@ -4,8 +4,19 @@ import prisma from "../prisma";
 import { HotelCreateRequest } from "./schema";
 
 export const hotelCreateController = async (req: HotelCreateRequest, res: Response, next: NextFunction) => {
+  const services = await prisma.service.findMany({})
   const hotel = await prisma.hotel.create({
-    data: req.body,
+    data: {
+      ...req.body,
+      services: {
+        createMany: {
+          data: services.map(v => ({
+            serviceId: v.id,
+            serviceRate: 0
+          }))
+        }
+      }
+    },
   })
 
   resp(res, hotel)
