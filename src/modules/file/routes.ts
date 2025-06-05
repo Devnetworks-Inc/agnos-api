ea7476a41d92c585c530b99e4a357c2f41c182c9
Michael Alegrado <alemike03@gmail.com>
2025-06-05 15:01:26 +0800

    up

--- src/modules/file/routes.ts
+++ src/modules/file/routes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express"
 import { validateToken } from "src/middlewares/validateToken"
-import { fileUploadController } from "./ctrl.post"
+import { fileUploadEmployeeFilesController, fileUploadProfilePicController } from "./ctrl.post"
 import { upload } from "src/middlewares/upload"
+import validateRequest from "src/middlewares/validateRequest"
+import { FileEmployeeUpload } from "./schema"
 
 const fileRouter = Router()
 
 fileRouter.use(validateToken)
-fileRouter.post('/upload', upload.single('file'), fileUploadController)
+
+fileRouter.post(
+  '/employee-profile/:employeeId',
+  validateRequest(FileEmployeeUpload),
+  upload.single('file'), fileUploadProfilePicController
+)
+
+fileRouter.post(
+  '/employee-files/:employeeId',
+  validateRequest(FileEmployeeUpload),
+  upload.array('files', 10), fileUploadEmployeeFilesController
+)
 
 export default fileRouter
